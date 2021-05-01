825bc7c36e9dd0a93f94ae791cf51a8ea67dba20
K1nan <ffo98@windowslive.com>
2021-05-01 18:49:10 +0200

    XML Design

---
+++ app/src/main/java/com/carpool/application/slutdestination.java
@@ -0,0 +1,14 @@
+package com.carpool.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class slutdestination extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slutdestination);
+    }
+}
