4d2da6b7b3dca64f952b8f16fd808e0d0b50e907
K1nan <ffo98@windowslive.com>
2021-05-10 19:46:00 +0200

    XML Design

--- app/src/main/java/com/carpool/application/sure.kt
+++ app/src/main/java/com/carpool/application/sure.kt
@@ -13,40 +13,57 @@ import androidx.appcompat.app.AppCompatActivity
 class sure : AppCompatActivity() {
     private lateinit var hundredsure: TextView
     private lateinit var from: EditText
-    private lateinit var to: TextView
-    private lateinit var number: TextView
-    private lateinit var depart: TextView
+    private lateinit var to: EditText
+    private lateinit var number: EditText
+    private lateinit var depart: EditText
     private lateinit var drive: Button
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sure)
-        hundredsure = findViewById(R.id.ethundredsure)
-        from = findViewById(R.id.etFrom)
-        to = findViewById(R.id.etTo)
-        number = findViewById(R.id.etNumber)
-        depart = findViewById(R.id.etDepart)
-        drive = findViewById<View>(R.id.drive) as Button
-        drive.setOnClickListener {
-            if (!hundredsure.getText().toString().isEmpty() && !!from.getText().toString()
-                    .isEmpty() && !to.getText().toString().isEmpty() &&
-                !number.getText().toString().isEmpty() && !depart.getText().toString().isEmpty()
-            ) {
-                val intent = Intent(Intent.ACTION_INSERT)
-                intent.type = ContactsContract.RawContacts.CONTENT_TYPE
-                intent.putExtra(
-                    ContactsContract.Intents.Insert.NAME,
-                    hundredsure.getText().toString()
+        hundredsure = findViewById(R.id.hundredsure)
+        from = findViewById(R.id.from)
+        to = findViewById(R.id.to)
+        number = findViewById(R.id.number)
+        depart = findViewById(R.id.depart)
+        drive = findViewById(R.id.drive)
+        /*drive.setOnClickListener(object : View.OnClickListener
+        {
+            override fun onClick(v: View)
+            {
+                if (!hundredsure.getText().toString().isEmpty() && !!from.getText().toString()
+                        .isEmpty() && !to.getText().toString().isEmpty() &&
+                    !number.getText().toString().isEmpty() && !depart.getText().toString()
+                        .isEmpty()
                 )
-                intent.putExtra(ContactsContract.Intents.Insert.NAME, from.getText().toString())
-                intent.putExtra(ContactsContract.Intents.Insert.NAME, to.getText().toString())
-                intent.putExtra(ContactsContract.Intents.Insert.NAME, number.getText().toString())
-                intent.putExtra(ContactsContract.Intents.Insert.NAME, depart.getText().toString())
-                if (intent.resolveActivity(packageManager) != null) {
-                    startActivity(intent)
+                {
+                    val intent = Intent(Intent.ACTION_INSERT)
+                    intent.type = ContactsContract.RawContacts.CONTENT_TYPE
+                    intent.putExtra(
+                        ContactsContract.Intents.Insert.NAME,
+                        hundredsure.getText().toString()
+                    )
+                    intent.putExtra(ContactsContract.Intents.Insert.NAME, from.getText().toString())
+                    intent.putExtra(ContactsContract.Intents.Insert.NAME, to.getText().toString())
+                    intent.putExtra(
+                        ContactsContract.Intents.Insert.NAME,
+                        number.getText().toString()
+                    )
+                    intent.putExtra(
+                        ContactsContract.Intents.Insert.NAME,
+                        depart.getText().toString()
+                    )
+                    if (intent.resolveActivity(packageManager) != null)
+                    {
+                        startActivity(intent)
+                    }
+                    when (v.id)
+                        {
+                            R.id.drive -> print ( "Nu finns du som en förare på listan:")
+
+                        }
                 }
-                val toast = Toast.makeText(applicationContext, "Nu finns du som en förare på listan:", Toast.LENGTH_SHORT)
-                toast.show()
             }
-        }
+        })*/
     }
 }
